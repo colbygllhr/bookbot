71e0c7f5c14affdd6ed5878efd1d1821e2036533
colbygllhr <cjg0033@mix.wvu.edu>
2024-07-28 10:51:01 -0400

    added the final methods to bookbot, generated report

--- main.py
+++ main.py
@@ -1,18 +1,50 @@
-
-
 def main():
-    book_path = "bookbot/books/frankenstein.txt"
+    print("Gathering data...")
+    book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    count = count_words(text)
-    print(count)
+    num_words = get_num_words(text)
     
+    print("Generating report...")
+    print(f"### Begin report of {book_path} ###")
+    print(f"{num_words} found in the document")
+    char_dict = count_characters(text)
+    # Convert to list of dictionaries
+    char_list = [{'char': k, 'num': v} for k, v in char_dict.items()]
+    char_list.sort(reverse=True, key=sort_on)
+
+    for i in char_list:
+        print(f"The '{i['char']}' character was found {i['num']} times")
+    
+    print("### End Report ###")
+
+
+def count_characters(text):
+    char_dict = {}
+    for letter in text:
+        if letter.isalpha():
+
+            letter = letter.lower()
+            if letter in char_dict:
+             char_dict[letter] += 1
+            else:
+             char_dict[letter] = 1
+    
+
+    return char_dict
+
+
+def sort_on(item):
+    return item["num"]
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-    
-def count_words(txt):
-   words = txt.split()
-        
+
 
 main()
